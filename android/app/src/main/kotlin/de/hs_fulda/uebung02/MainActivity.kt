ef52a05880868b8d658f29fc9653b27e82dd91f1
tabea.runzheimer@informatik.hs-fulda.de <tabea.runzheimer@informatik.hs-fulda.de>
2019-10-29 15:10:05 +0100

    initial commit

---
+++ android/app/src/main/kotlin/de/hs_fulda/uebung02/MainActivity.kt
@@ -0,0 +1,13 @@
+package de.hs_fulda.uebung02
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
