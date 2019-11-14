a1cc19ede97e87811d1bfbf8a098994b848d1e13
tabea.runzheimer@informatik.hs-fulda.de <tabea.runzheimer@informatik.hs-fulda.de>
2019-11-14 19:45:20 +0100

    Delete Row

--- lib/screens/done_workout_screen.dart
+++ lib/screens/done_workout_screen.dart
@@ -22,7 +22,76 @@ class _DoneWorkoutScreenState extends State<DoneWorkoutScreen> {
         ],
       ),
       body: Container(
-        color: Colors.black,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              ///Hintergrundbild
+              child: new Image.asset(
+                'assets/img/female_boxer.jpg',
+                fit: BoxFit.fill,
+                height: 900.0,
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: (MediaQuery.of(context).size.height / 2.5),
+              margin: EdgeInsets.only(top: (MediaQuery.of(context).size.height / 5)),
+              //color: Colors.red,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text("Gut gemacht", style: Theme.of(context).textTheme.headline,),
+                  Divider(
+                    color: Colors.white,
+                    indent: 120,
+                    endIndent: 120,
+                    thickness: 2,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 80, right: 80),
+                    child: Text("Nimm dir nach jedem Workout etwas Zeit um dich zu dehnen", textAlign: TextAlign.center,),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              margin: EdgeInsets.fromLTRB(0, (MediaQuery.of(context).size.height - 200), 0, 0),
+              //color: Colors.green,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width/3.5,
+                    child: Text("kcal\n180",textAlign: TextAlign.center,),
+                  ),
+                  VerticalDivider(
+                    indent: 20,
+                    endIndent: 20,
+                    color: Colors.white,
+                    thickness: 2,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width/3.5,
+                    child:  Text("Zeit\n180 sek",textAlign: TextAlign.center,),
+                  ),
+                  VerticalDivider(
+                    indent: 20,
+                    endIndent: 20,
+                    color: Colors.white,
+                    thickness: 2,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width/3.5,
+                    child: Text("Bewertung\n 4",textAlign: TextAlign.center,),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
